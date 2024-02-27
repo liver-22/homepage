e54229cc003fc705bbf66b435bc17a02287f777e
Krzysztof Watroba <krzysiak89@gmail.com>
2024-02-28 00:23:51 +0100

    refactor script.js (added functions)

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
-let buttonElement = document.querySelector(".js-buttonChangeTeam");
-let imgUltimateElement = document.querySelector(".js-imgUltimateTeam");
-let imgUltimatePlElement = document.querySelector(".js-imgUltimateTeamPL");
-let teamNameElement = document.querySelector(".js-teamName");
+{
+const toggleUltimateImage = () => {
+    const imgUltimateElement = document.querySelector(".js-imgUltimateTeam");
+    const imgUltimatePlElement = document.querySelector(".js-imgUltimateTeamPL");
+    const teamNameElement = document.querySelector(".js-teamName");
 
-buttonElement.addEventListener("click", () => {
     imgUltimateElement.classList.toggle("section__image--hidden");
     imgUltimatePlElement.classList.toggle("section__image--hidden");
     teamNameElement.innerText = imgUltimateElement.classList.contains("section__image--hidden") ? "Amerykańską" : "Polską" ;
-});
+};
+
+const init = () => {
+    const buttonElement = document.querySelector(".js-buttonChangeTeam");
+    buttonElement.addEventListener("click", toggleUltimateImage);
+};
+
+init();
+
+}

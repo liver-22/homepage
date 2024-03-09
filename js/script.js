7323feefc61577e1ce72702bde1cb1a0c5f6b39c
Krzysztof Watroba <krzysiak89@gmail.com>
2024-03-09 14:59:29 +0100

    modified code with prettier

--- js/script.js
+++ js/script.js
@@ -1,19 +1,18 @@
 {
-const toggleUltimateImage = () => {
-    const imgUltimateElement = document.querySelector(".js-imgUltimateTeam");
-    const imgUltimatePlElement = document.querySelector(".js-imgUltimateTeamPL");
-    const teamNameElement = document.querySelector(".js-teamName");
+	const toggleUltimateImage = () => {
+		const imgUltimateElement = document.querySelector(".js-imgUltimateTeam");
+		const imgUltimatePlElement = document.querySelector(".js-imgUltimateTeamPL");
+		const teamNameElement = document.querySelector(".js-teamName");
 
-    imgUltimateElement.classList.toggle("section__image--hidden");
-    imgUltimatePlElement.classList.toggle("section__image--hidden");
-    teamNameElement.innerText = imgUltimateElement.classList.contains("section__image--hidden") ? "Amerykańską" : "Polską" ;
-};
+		imgUltimateElement.classList.toggle("section__image--hidden");
+		imgUltimatePlElement.classList.toggle("section__image--hidden");
+		teamNameElement.innerText = imgUltimateElement.classList.contains("section__image--hidden") ? "Amerykańską" : "Polską";
+	};
 
-const init = () => {
-    const buttonElement = document.querySelector(".js-buttonChangeTeam");
-    buttonElement.addEventListener("click", toggleUltimateImage);
-};
+	const init = () => {
+		const buttonElement = document.querySelector(".js-buttonChangeTeam");
+		buttonElement.addEventListener("click", toggleUltimateImage);
+	};
 
-init();
-
+	init();
 }
